c6232b413403866181f96757ebb436b6b7417970
Charley Gooch <chazgooch@gmail.com>
2018-10-08 14:51:12 +0000

    cleaning up formatting

--- spec/Oystercard_Spec.rb
+++ spec/Oystercard_Spec.rb
@@ -7,23 +7,30 @@ describe Oystercard do
   before do
     @oystercard = Oystercard.new()
   end
-  it 'initializes with a balance of 0' do
-    expect(@oystercard.getBalance()).to eq(0)
+  describe '#topUp' do
+    it 'initializes with a balance of 0' do
+      expect(@oystercard.getBalance()).to eq(0)
+    end
+    it 'responds to method topUp' do
+      expect(@oystercard).to respond_to(:topUp)
+    end
+    it 'adds value passed to topUp to balance' do
+      @oystercard.topUp(50)
+      expect(@oystercard.getBalance()).to eq(50)
+    end
+    it 'raises an exception if sum increases balance above 90'do
+      @oystercard.topUp(90)
+      expect{@oystercard.topUp(1)}.to raise_error(BalanceOverLimitException,
+      "Maximum balance of #{@oystercard.getMaxBalance()} exceeded")
+    end
   end
-  it 'responds to method top_up' do
-    expect(@oystercard).to respond_to(:topUp)
-  end
-  it 'adds value passed to topUp to balance' do
-    @oystercard.topUp(50)
-    expect(@oystercard.getBalance()).to eq(50)
-  end
-  it 'raises an exception if sum increases balance above 90'do
-    @oystercard.topUp(90)
-    expect{@oystercard.topUp(1)}.to raise_error(BalanceOverLimitException,
-    "Maximum balance of #{@oystercard.getMaxBalance()} exceeded")
-  end
-  it 'raises an exception if deduction brings balance below 0' do
-    expect{@oystercard.deduct(10)}.to raise_error(BalanceUnderLimitException,
-    "Minimum Balance of #{@oystercard.getMinBalance()} exceeded")
+  describe '#deduct' do
+    it 'responds to method deduct' do
+      expect(@oystercard).to respond_to(:deduct)
+    end
+    it 'raises an exception if deduction brings balance below 0' do
+      expect{@oystercard.deduct(10)}.to raise_error(BalanceUnderLimitException,
+      "Minimum Balance of #{@oystercard.getMinBalance()} exceeded")
+    end
   end
 end
